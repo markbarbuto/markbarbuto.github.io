89f368bffd8c35f07edb812c22854e36222740e7
Mark Barbuto <markbarbuto@outlook.com>
2025-01-12 21:41:13 -0500

    added email page

--- src/views/contact/contact.vue
+++ src/views/contact/contact.vue
@@ -5,9 +5,24 @@
     </div>
     <div class="contact-content-container">
       <div class="contact-content">
-        <span class="contact-description">
-          peepee
-        </span>
+        <div class="contact-email">
+          markbarbuto@outlook.com
+          <a href="mailto:markbarbuto@outlook.com" class="form-send-button" type="submit">email me</a>
+        </div>
+        <!-- <form class="contact-form">
+          <p>
+            <label for="email">email</label>
+            <input v-model="email" class="form-email" placeholder="example@test.com" />
+          </p>
+
+          <p>
+            <label for="message">message</label>
+            <textarea v-model="message" id="message" class="form-message" type="text" placeholder="send me a message"></textarea>
+          </p>
+
+          <a href="mailto:markbarbuto@outlook.com" class="form-send-button" type="submit">send</a>
+        </form> -->
+        <img class="contact-image" src="/assets/succulent.png" alt="mark" />
       </div>
     </div>
   </div>
